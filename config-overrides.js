fc79f94742a9fee70492ebf8d6f2dc4be304ce2b
xiaotian <yangzhoulty2008@outlook.com>
2022-01-25 19:32:09 +0800

    chore: format code

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,4 @@
-const path = require("path");
+const path = require('path');
 
 const {
   override,
@@ -7,37 +7,25 @@ const {
   addPostcssPlugins,
   addWebpackResolve,
   addWebpackModuleRule,
-} = require("customize-cra");
+} = require('customize-cra');
 module.exports = override(
   addDecoratorsLegacy(),
   addWebpackAlias({
-    api: path.resolve(__dirname, "./src/api"),
-    mobxStore: path.resolve(__dirname, "./src/store/mobxStore"),
-    reduxStore: path.resolve(__dirname, "./src/store/reduxStore"),
-    reduxToolkitStore: path.resolve(__dirname, "./src/store/reduxToolkitStore"),
-    recoilStore: path.resolve(__dirname, "./src/store/recoilStore"),
-    components: path.resolve(__dirname, "./src/components"),
-    pages: path.resolve(__dirname, "./src/pages"),
-    router: path.resolve(__dirname, "./src/router"),
-    assets: path.resolve(__dirname, "./src/assets"),
-    utils: path.resolve(__dirname, "./src/shared/utils"),
-    styles: path.resolve(__dirname, "./src/shared/styles"),
+    api: path.resolve(__dirname, './src/api'),
+    mobxStore: path.resolve(__dirname, './src/store/mobxStore'),
+    reduxStore: path.resolve(__dirname, './src/store/reduxStore'),
+    reduxToolkitStore: path.resolve(__dirname, './src/store/reduxToolkitStore'),
+    recoilStore: path.resolve(__dirname, './src/store/recoilStore'),
+    components: path.resolve(__dirname, './src/components'),
+    pages: path.resolve(__dirname, './src/pages'),
+    router: path.resolve(__dirname, './src/router'),
+    assets: path.resolve(__dirname, './src/assets'),
+    utils: path.resolve(__dirname, './src/shared/utils'),
+    styles: path.resolve(__dirname, './src/shared/styles'),
   }),
   addWebpackResolve({
-    extensions: [
-      ".js",
-      ".ts",
-      "jsx",
-      "tsx",
-      ".json",
-      ".wasm",
-      ".css",
-      ".less",
-      ".sass",
-      ".scss",
-      ".styl",
-    ],
+    extensions: ['.js', '.ts', 'jsx', 'tsx', '.json', '.wasm', '.css', '.less', '.sass', '.scss', '.styl'],
   }),
-  addPostcssPlugins([require("tailwindcss"), require("autoprefixer")]),
-  addWebpackModuleRule({ test: /\.png$/, use: "url-loader" })
+  addPostcssPlugins([require('tailwindcss'), require('autoprefixer')]),
+  addWebpackModuleRule({ test: /\.png$/, use: 'url-loader' }),
 );
